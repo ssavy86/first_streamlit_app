ee2f28f4ce307d9749c0f41a87857d72d6f704b3
Steve <114014166+ssavy86@users.noreply.github.com>
2022-09-20 14:25:45 -0400

    Update streamlit_app.py
    Added new commands in second try

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+
+streamlit.header('Breakfast Menu')
+
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+
+streamlit.title('Hard-boiled Free-Range Egg')
