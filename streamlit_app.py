ab4beaefb603b4e479415979ca861b857edc9eb3
Steve <114014166+ssavy86@users.noreply.github.com>
2022-09-22 12:01:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,7 @@ import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
+
 # streamlit.text(fruityvice_response.json()) - deleted per instructions
 
 # write your own comment -what does the next line do? 
